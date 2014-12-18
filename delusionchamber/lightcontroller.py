0de59a0ffb57f456c3c8e21d9d2b6039d21e029d
Jake Schievink <jakeschievink@gmail.com>
2014-12-17 20:34:28 -0500

    Added

---
+++ delusionchamber/lightcontroller.py
@@ -0,0 +1,26 @@
+RED_PIN = 17
+GREEN_PIN = 27
+BLUE_PIN = 22
+
+class LightController():
+    def __init__(self, GPIO, r, g, b):
+        self.GPIO = GPIO
+        self.red = GPIO.PWM(r, 100)
+        self.green = GPIO.PWM(g, 100)
+        self.blue = GPIO.PWM(b, 100)
+    def set_light(self, r, g, b):
+    def fade_red(self):
+        self.red.start(0)
+        for i in range(0, 101):
+            selfred.ChangeDutyCycle(i)
+            sleep(0.02)
+        for i in range(100, -1, -1):
+            self.red.ChangeDutyCycle(i)
+            sleep(0.02)
+
+
+
+
+
+
+
--- delusionchamber/main.py
+++ delusionchamber/main.py
@@ -1,16 +1,28 @@
+import RPi.GPIO as GPIO
 from randomaudio import RandomAudio
+from lightcontroller import LightController
 from pygame import mixer
+RED_PIN = 17
+GREEN_PIN = 27
+BLUE_PIN = 22
+
+MOTION_PIN = 18
+
 class DelusionChamber:
     def __init__(self):
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(MOTION_PIN, GPIO.IN)
         self.pm = mixer
         self.pm.init()
         self.random_ideas = RandomAudio('../idea_files', self.pm, 1, 0.2)
         self.random_music = RandomAudio('../music_files', self.pm, 2, 0.7)
-
+        self.light = LightController(GPIO, RED_PIN, GREEN_PIN, BLUE_PIN)
     def start(self):
         while True:
-            self.random_music.play_sound()
-            self.random_ideas.play_sound()
+            if GPIO.input(18)
+                self.light.fade_red()
+            # self.random_music.play_sound()
+            # self.random_ideas.play_sound()
 
 
 
